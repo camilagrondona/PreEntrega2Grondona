57750e12c81067518b26d9c4dab92cae2b2c5f0e
Camila Grondona <grondonacamila@gmail.com>
2023-08-11 16:54:22 +0200

    bucle find

--- js/main.js
+++ js/main.js
@@ -1,59 +1,65 @@
 const planes = [{
-        id: 1,
-        nombre: "PLAN A",
-        precio: 3630
-    },
-    {
-        id: 2,
-        nombre: "PLAN B",
-        precio: 4840
-    },
-    {
-        id: 3,
-        nombre: "PLAN C",
-        precio: 6050
-    },
-    {
-        id: 4,
-        nombre: "PLAN D",
-        precio: 7260
-    },
+    id: 1,
+    nombre: "PLAN A",
+    precio: 3630
+},
+{
+    id: 2,
+    nombre: "PLAN B",
+    precio: 4840
+},
+{
+    id: 3,
+    nombre: "PLAN C",
+    precio: 6050
+},
+{
+    id: 4,
+    nombre: "PLAN D",
+    precio: 7260
+},
 ];
 
 // Find 
 
-let nombre = prompt("Ingrese el nombre del plan que desea verificar").toUpperCase();
-let plan = planes.find((plan) => plan.nombre === nombre);
+let inputValido = true;
+while (inputValido) {
 
-if (plan) {
-    alert
-        (`
+    let nombre = prompt("Ingrese el nombre del plan que desea verificar").toUpperCase();
+    let plan = planes.find((plan) => plan.nombre === nombre);
+
+    if (plan) {
+        alert
+            (`
 Id: ${plan.id}
 Nombre: ${plan.nombre}
 Precio: ${plan.precio}
 `);
-} else {
-    alert("El plan que ingresaste no se encuentra disponible");
+        inputValido = false; // Valor ingresado es válido, salir del bucle
+    } else {
+        alert("El plan que ingresaste no se encuentra disponible, intenta de nuevo.");
+    }
 }
 
 // Filter
 
 let validInput = true;
 while (validInput) {
-let precio = parseInt(prompt("Ingresá el precio máximo que estás dispuesto/a a abonar para ver cuál de nuestros planes se ajusta mejor a vos"))
-let filtrados = planes.filter(plan => plan.precio < precio)
 
-if (filtrados.length > 0){
-filtrados.forEach(plan => {
-    alert(`
+    let precio = parseInt(prompt("Ingresá el precio máximo que estás dispuesto/a a abonar para ver cuál de nuestros planes se ajusta mejor a vos"))
+    let filtrados = planes.filter(plan => plan.precio < precio)
+
+    if (filtrados.length > 0) {
+        filtrados.forEach(plan => {
+            alert(`
     Id: ${plan.id}
     Nombre: ${plan.nombre}
     Precio: ${plan.precio}
     `);
-});
-validInput = false; // Valor ingresado es válido, salir del bucle
-} else {
-    alert ("El valor ingresado no se ajusta a ninguno de los planes disponibles. Por favor, intenta nuevamente.");
-}
+        });
+        validInput = false; // Valor ingresado es válido, salir del bucle
+    } else {
+        alert("El valor ingresado no se ajusta a ninguno de los planes disponibles. Por favor, intenta nuevamente.");
+    }
 }
 
