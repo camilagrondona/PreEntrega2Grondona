380439d3206b5abe5ebb6068ef50fd8e5355c7c2
Camila Grondona <grondonacamila@gmail.com>
2023-08-11 16:40:38 +0200

    bucle filter

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
-const planes = [
-    { id: 1, nombre: "PLAN A", precio: 3630 },
-    { id: 2, nombre: "PLAN B", precio: 4840 },
-    { id: 3, nombre: "PLAN C", precio: 6050 },
-    { id: 4, nombre: "PLAN D", precio: 7260 },
+const planes = [{
+        id: 1,
+        nombre: "PLAN A",
+        precio: 3630
+    },
+    {
+        id: 2,
+        nombre: "PLAN B",
+        precio: 4840
+    },
+    {
+        id: 3,
+        nombre: "PLAN C",
+        precio: 6050
+    },
+    {
+        id: 4,
+        nombre: "PLAN D",
+        precio: 7260
+    },
 ];
 
 // Find 
@@ -23,16 +38,22 @@ Precio: ${plan.precio}
 
 // Filter
 
+let validInput = true;
+while (validInput) {
 let precio = parseInt(prompt("Ingresá el precio máximo que estás dispuesto/a a abonar para ver cuál de nuestros planes se ajusta mejor a vos"))
 let filtrados = planes.filter(plan => plan.precio < precio)
 
+if (filtrados.length > 0){
 filtrados.forEach(plan => {
     alert(`
     Id: ${plan.id}
     Nombre: ${plan.nombre}
     Precio: ${plan.precio}
-    `)
+    `);
+});
+validInput = false; // Valor ingresado es válido, salir del bucle
+} else {
+    alert ("El valor ingresado no se ajusta a ninguno de los planes disponibles. Por favor, intenta nuevamente.");
 }
-)
-
+}
 
